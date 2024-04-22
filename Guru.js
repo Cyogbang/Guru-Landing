b697e7ae377091ebb97a62e4bf5156b4a1c438d1
Cy <ogbangcyprian360@gmail.com>
2024-04-23 00:59:38 +0100

    JS fixed, search icon working now

--- Guru.js
+++ Guru.js
@@ -13,19 +13,59 @@
 // }
 
 function highlight1() {
-    const home = document.getElementById ("about1");
+    const home = document.querySelector (".about1");
 
-    if (home.style.display = "none") {
+    if (home.style.display === "none") {
         home.style.display = "block";
     } else {
         home.style.display = "none";
     }
 }
 
+function highlight2() {
+    const about = document.querySelector (".about2");
+
+    if (about.style.display === "none") {
+        about.style.display = "block";
+    } else {
+        about.style.display = "none";
+    }
+}
+
+function highlight3() {
+    const service = document.querySelector (".about3");
+
+    if (service.style.display === "none") {
+        service.style.display = "block";
+    } else {
+        service.style.display = "none";
+    }
+}
+
+function highlight4() {
+    const data = document.querySelector (".about4");
+
+    if (data.style.display === "none") {
+        data.style.display = "block";
+    } else {
+        data.style.display = "none";
+    }
+}
+
+function highlight5() {
+    const client = document.querySelector (".about5");
+
+    if (client.style.display === "none") {
+        client.style.display = "block";
+    } else {
+        client.style.display = "none";
+    }
+}
+
 function searchDisplay() {
     const search = document.getElementById ("searchForm");
 
-    if (search.style.display = "none") {
+    if (search.style.display === "none") {
         console.log("none");
         search.style.display = 'block';
     } else {
@@ -33,3 +73,7 @@ function searchDisplay() {
         search.style.display = 'none';
     } 
 }
+
+// function searchDisplay () {
+//     document.getElementById("searchForm").classList.toggle("hidden");
+// }
